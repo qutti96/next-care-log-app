e73260829a08de9d95a680aa0328bf125f941b35
qutti96 <qutti96@gmail.com>
2025-07-16 09:59:18 +0900

    - supabase クライアントを初期化 - 最新ER図に基づくPrismaスキーマに置き換え

---
+++ src/app/lib/supabase.ts
@@ -0,0 +1,8 @@
+//supabaseクライアントを初期化
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
